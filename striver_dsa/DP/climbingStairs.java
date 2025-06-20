684d8ff3316f90a6c26b552c0b8a04a362e1ffed
Santhosh M <santhoshsantum123@gmail.com>
2025-06-20 23:53:45 +0530

    added newfile

---
+++ striver_dsa/DP/climbingStairs.java
@@ -0,0 +1,22 @@
+package DP;
+import java.util.*;
+public class climbingStairs{
+    static int res(int n){
+        if(n<=2){
+            return n;
+        }
+        int[] dp=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(res(n));
+
+    }
+}

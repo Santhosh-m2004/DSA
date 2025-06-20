130fc1b16d47171b1fea24454c27faf4b4f67a72
Santhosh M <santhoshsantum123@gmail.com>
2025-06-21 00:12:40 +0530

    added frogJump

--- striver_dsa/DP/climbingStairs.java
+++ striver_dsa/DP/climbingStairs.java
@@ -1,4 +1,6 @@
 package DP;
+/*You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top? */
 import java.util.*;
 public class climbingStairs{
     static int res(int n){
